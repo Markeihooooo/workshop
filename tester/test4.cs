e172e90146901d78bf0cba00001813e3285d77d6
Saravut hungsakunrat <66111310@dpu.ac.th>
2024-02-03 21:35:53 +0700

    test del

--- tester/test4.cs
+++ tester/test4.cs
@@ -1,52 +1,52 @@
-﻿using System;
-namespace tester
-{
-	public class test4
-	{
-		public static void Main(string[]agrs )
-		{
-			int selectnumber;
-			int basenumber;
-			int heightnumber;
-			int radius;
+﻿//using System;
+//namespace tester
+//{
+//	public class test4
+//	{
+//		public static void Main(string[] agrs)
+//		{
+//			int selectnumber;
+//			int basenumber;
+//			int heightnumber;
+//			int radius;
 
 
-			Console.WriteLine("1: Rectangle \n2:Triangle \n3: Circle");
+//			Console.WriteLine("1: Rectangle \n2:Triangle \n3: Circle");
 
-			Console.Write("Please select shape to calculate area :");
-			selectnumber = Convert.ToInt32(Console.ReadLine());
+//			Console.Write("Please select shape to calculate area :");
+//			selectnumber = Convert.ToInt32(Console.ReadLine());
 
-			if (selectnumber == 1)
-			{
-                Console.Write("Please enter base  :");
-                basenumber = Convert.ToInt32(Console.ReadLine());
-                Console.Write("please enter height : ");
-                heightnumber = Convert.ToInt32(Console.ReadLine());
+//			if (selectnumber == 1)
+//			{
+//				Console.Write("Please enter base  :");
+//				basenumber = Convert.ToInt32(Console.ReadLine());
+//				Console.Write("please enter height : ");
+//				heightnumber = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Area = {0}", basenumber * heightnumber);
+//				Console.WriteLine("Area = {0}", basenumber * heightnumber);
 
 
-            }
-            else if (selectnumber == 2)
-			{
-				Console.Write("Please enter base  :");
-                basenumber = Convert.ToInt32(Console.ReadLine());
-				Console.Write("please enter height : ");
-				heightnumber = Convert.ToInt32(Console.ReadLine());
+//			}
+//			else if (selectnumber == 2)
+//			{
+//				Console.Write("Please enter base  :");
+//				basenumber = Convert.ToInt32(Console.ReadLine());
+//				Console.Write("please enter height : ");
+//				heightnumber = Convert.ToInt32(Console.ReadLine());
 
-				Console.WriteLine("Area = {0}", (0.5 * basenumber)*heightnumber);
+//				Console.WriteLine("Area = {0}", (0.5 * basenumber) * heightnumber);
 
-			}
-			else if (selectnumber == 3)
-			{
-                Console.Write("please enter height : ");
-                radius = Convert.ToInt32(Console.ReadLine());
+//			}
+//			else if (selectnumber == 3)
+//			{
+//				Console.Write("please enter height : ");
+//				radius = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Area = {0:N2}",Math.PI*Math.Pow(radius,2));
+//				Console.WriteLine("Area = {0:N2}", Math.PI * Math.Pow(radius, 2));
 
-            }
-            else Console.WriteLine("Selected number must be 1 - 3");
-		}
-	}
-}
+//			}
+//			else Console.WriteLine("Selected number must be 1 - 3");
+//		}
+//	}
+//}
 
